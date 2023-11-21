fbb0f23c095d8e47f12aca628b2669b2dcc5cfc7
your Name <you@example.com>
2023-11-21 22:20:53 +0700

    first push

--- JS/index.js
+++ JS/index.js
@@ -29,6 +29,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
+  
 }
 
 function validateForm(){
