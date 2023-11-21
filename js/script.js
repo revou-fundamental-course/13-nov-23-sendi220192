b15e394f732b1c6d7fc755b7c33906b952523840
your Name <you@example.com>
2023-11-22 00:01:07 +0700

    first push

--- js/script.js
+++ js/script.js
@@ -34,21 +34,23 @@ function showSlides(n) {
 
 function validateForm(){
   const name = document.forms["message-form"]["full-name"].value;
+  const email = document.forms["message-form"]["email"].value;
   const birthDate = document.forms["message-form"]["birth-date"].value;
   const gender = document.forms["message-form"]["gender"].value;
   const messages = document.forms["message-form"]["messages"].value;
 
-  if (name == "" || birthDate == "" || gender == "" || messages == ""){
+  if (name == "" || email == "" || birthDate == "" || gender == "" || messages == ""){
     alert("Tidak boleh ada yang kosong");
     return false;
   }
-  setSenderUI(name, birthDate, gender, messages);
+  setSenderUI(name, email, birthDate, gender, messages);
   return false;
 
 }
 
-function setSenderUI(name, birthDate, gender, messages){
+function setSenderUI(name, email, birthDate, gender, messages){
   document.getElementById("sender-full-name").innerHTML = name;
+  document.getElementById("sender-email").innerHTML = email;
   document.getElementById("sender-birth-date").innerHTML = birthDate;
   document.getElementById("sender-gender").innerHTML = gender;
   document.getElementById("sender-messages").innerHTML = messages;
