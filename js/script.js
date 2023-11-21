aa6c55ad3c7a7adecd9adf782897a1b79ac35e3b
your Name <you@example.com>
2023-11-21 23:11:30 +0700

    first push

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ function validateForm(){
   const name = document.forms["message-form"]["full-name"].value;
   const birthDate = document.forms["message-form"]["birth-date"].value;
   const gender = document.forms["message-form"]["gender"].value;
-  const messages = document.forms["message-form"]["message"].value;
+  const messages = document.forms["message-form"]["messages"].value;
 
   if (name == "" || birthDate == "" || gender == "" || messages == ""){
     alert("Tidak boleh ada yang kosong");
